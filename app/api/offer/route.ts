8fe2a065f0c5d5f818cda6c41fd7b290a6462135
YasserAbdalla <yasserabdalla149@gmail.com>
2024-05-06 23:42:20 +0300

    New json

--- app/api/offer/route.ts
+++ app/api/offer/route.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises"; // Use fs promises API for async/await support
 
 export async function GET(req: NextRequest) {
   try {
-    const data = await fs.readFile("./types/data.json", "utf-8");
+    const data = await fs.readFile("./types/NewData.json", "utf-8");
     return NextResponse.json({ offers: JSON.parse(data) });
   } catch (err: any) {
     console.error("Error reading JSON file:", err);
