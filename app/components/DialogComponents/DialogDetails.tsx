d8b82062b83847399aa96daf6b5455e874b3848a
YoussefMassoud <youssefbusiness133@gmail.com>
2024-05-07 17:17:41 +0300

    first color dialog

--- app/components/DialogComponents/DialogDetails.tsx
+++ app/components/DialogComponents/DialogDetails.tsx
@@ -20,46 +20,46 @@ const DialogDetails = ({
                 <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
                   {index + 1}
                 </p>
-                <h1 className="text-white text-xl">{data}</h1>
+                <h1 className="text-secondary-foreground text-xl">{data}</h1>
               </div>
             ))}
           {!instructions_array && (
             <div className="flex flex-col  gap-8">
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {1}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {2}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {3}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {4}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {5}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
               <div className="flex gap-3">
-                <p className="rounded-full border-2 w-fit h-fit px-4 bg-gray-300 text-white text-2xl">
+                <p className="rounded-full border-2 w-fit h-fit px-4 bg-secondary text-secondary-foreground text-2xl">
                   {6}
                 </p>
-                <h1 className="text-white text-xl">{instructions}</h1>
+                <h1 className="text-primary text-xl">{instructions}</h1>
               </div>
             </div>
           )}
