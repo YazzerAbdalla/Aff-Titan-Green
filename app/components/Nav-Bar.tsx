34325f5971aefac88ea6bd988bee9196b005916d
YoussefMassoud <youssefbusiness133@gmail.com>
2024-05-07 16:57:46 +0300

    change color in survey ?&& card

--- app/components/Nav-Bar.tsx
+++ app/components/Nav-Bar.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-[#16A34A]">
+    <div className="fixed top-0 left-0 w-full z-50 bg-primary">
       <div className="flex justify-between lg:px-48 py-7">
         <div className="flex items-center px-3  lg:px-16 justify-start">
           <span className="text-green-400 text-3xl font-bold">
-            Aff<span className="text-white px-1">Titan</span>
+            Aff<span className="text-primary-foreground  px-1">Titan</span>
           </span>
-          <span className="text-[#FFF1F2] px-8 mt-2 font-medium  hidden lg:block">
+          <span className="text-primary-foreground px-8 mt-2 font-medium  hidden lg:block">
             Get Coins
           </span>
         </div>
 
         <div className="flex items-center  lg:px-16 justify-end">
-          <div className="flex px-4 bg-black rounded-3xl py-1">
+          <div className="flex px-4 bg-secondary-foreground rounded-3xl py-1">
             <img src="/money.png" alt="money" />
-            <span className="text-white px-2">0</span>
+            <span className="text-primary-foreground px-2">0</span>
           </div>
           <Link href="https://freecash.com/w/revu">
             <div className="px-5 cursor-pointer">
