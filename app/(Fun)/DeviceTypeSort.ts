b704ba7adfd13bbc0b84337b6f97d83683b6c62b
YasserAbdalla <yasserabdalla149@gmail.com>
2024-05-11 02:05:49 +0300

    fix the os function

--- app/(Fun)/DeviceTypeSort.ts
+++ app/(Fun)/DeviceTypeSort.ts
@@ -1,28 +1,35 @@
 import { CardsProps } from "@/types/cardsTypes";
 
-export function DeviceTypeSort(arr: CardsProps[], filterKind: string) {
+export function DeviceTypeSort(
+  arr: CardsProps[],
+  filterKind: string
+): CardsProps[] {
   const newArr = arr.map((item) => ({ ...item }));
 
   // Sort the array
   newArr.sort((a, b) => {
     // If the os of a contains the filterKind but not b, place a before b
-    if (
-      a.os.includes(filterKind.toLowerCase()) &&
-      !b.os.includes(filterKind.toLowerCase())
-    ) {
-      return -1;
+    if (a.os && b.os) {
+      if (
+        a.os.includes(filterKind.toLowerCase()) &&
+        !b.os.includes(filterKind.toLowerCase())
+      ) {
+        return -1;
+      }
+      // If the os of b contains the filterKind but not a, place b before a
+      else if (
+        !a.os.includes(filterKind.toLowerCase()) &&
+        b.os.includes(filterKind.toLowerCase())
+      ) {
+        return 1;
+      }
+      // Otherwise, keep the order unchanged
+      else {
+        return 0;
+      }
     }
-    // If the os of b contains the filterKind but not a, place b before a
-    else if (
-      !a.os.includes(filterKind.toLowerCase()) &&
-      b.os.includes(filterKind.toLowerCase())
-    ) {
-      return 1;
-    }
-    // Otherwise, keep the order unchanged
-    else {
-      return 0;
-    }
+    // Return 0 if a.os or b.os is undefined
+    return 0;
   });
 
   // Return the sorted array
