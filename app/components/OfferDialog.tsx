ebf623eb39a5babb80f753d1b9905e6923691217
YoussefMassoud <youssefbusiness133@gmail.com>
2024-05-11 17:00:53 +0300

    fixed button in small screen

--- app/components/OfferDialog.tsx
+++ app/components/OfferDialog.tsx
@@ -63,11 +63,11 @@ const OfferDialog = ({
             <div className="w-full flex justify-center absolute -bottom-0 lg:-bottom-20 px-6">
               <a
                 target="_blank"
-                className=" flex justify-center bg-[#16a34a] items-center text-white font-bold text-md py-1 px-16 rounded-xl max-w-[250px] min-w-[250px] min-h-[30px] max-h-[30px] lg:max-w-[460px] lg:min-w-[460px] lg:min-h-[39px] lg:max-h-[39px]"
+                className=" flex justify-center bg-[#16a34a] items-center text-white font-bold text-md py-1 px-16 rounded-xl max-w-[270px] min-w-[250px] min-h-[30px] max-h-[30px] lg:max-w-[460px] lg:min-w-[460px] lg:min-h-[39px] lg:max-h-[39px]"
                 href={url}
               >
                 {`Earn ${reward}  `}
-                <img src={"/coinIcon.png"} alt="" width={20} height={20} />
+                <img src={"/coinIcon.png"} alt="" width={20} height={20} className="hidden lg:block" />
                 {`  Coins `}
               </a>
             </div>
